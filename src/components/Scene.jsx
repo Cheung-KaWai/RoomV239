00037681784b49d26abd5f1d341f6be7c557db2a
Cheung-KaWai <84151954+Cheung-KaWai@users.noreply.github.com>
2023-07-09 18:44:35 +0200

    Add loader

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,6 +1,6 @@
-import { Box, Environment } from "@react-three/drei";
+import { Box, Environment, Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React from "react";
+import React, { Suspense } from "react";
 import { styled } from "styled-components";
 import { RoomV239 } from "./RoomV239";
 import { Desk } from "./Desk";
@@ -17,17 +17,20 @@ export const Scene = () => {
         flat
         legacy
       >
-        <Controls />
-        <Environment preset="studio" />
-        <group rotation-y={-Math.PI / 2}>
-          <RoomV239 />
-          <Desk />
-          <Pc />
-          <Chair />
-          <Painting />
-        </group>
-        <Curtain />
+        <Suspense fallback={null}>
+          <Controls />
+          <Environment preset="studio" />
+          <group rotation-y={-Math.PI / 2}>
+            <RoomV239 />
+            <Desk />
+            <Pc />
+            <Chair />
+            <Painting />
+          </group>
+          <Curtain />
+        </Suspense>
       </Canvas>
+      <Loader />
     </CanvasContainer>
   );
 };
@@ -35,4 +38,5 @@ export const Scene = () => {
 const CanvasContainer = styled.div`
   width: 100vw;
   height: 100vh;
+  background-color: #161616;
 `;
